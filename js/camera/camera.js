5ff4ca942d40f6a1df601d84e0d4f97ec5afa965
Wouter van Heeswijk <woutervh@gmail.com>
2015-07-19 13:38:53 +0200

    Added camera object

---
+++ js/camera/camera.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2015 Wouter van Heeswijk
+ */
+
+define([], function () {
+        var pitch = 0.0;
+        var yaw = 0.0;
+        var eye = vec3.fromValues(0.0, 0.0, 0.0);
+        var center = vec3.fromValues(0.0, 0.0, -1.0);
+        var up = vec3.fromValues(0.0, 1.0, 0.0);
+        var viewMatrix = mat4.create();
+
+        return {
+            getViewMatrix: function () {
+                var sp = Math.sin(pitch);
+                var cp = Math.cos(pitch);
+                var sy = Math.sin(yaw);
+                var cy = Math.cos(yaw);
+                center[0] = -cp * sy;
+                center[1] = sp;
+                center[2] = cp * cy
+                mat4.lookAt(viewMatrix, eye, center, up);
+                return viewMatrix;
+            },
+
+            increasePitch: function (amount) {
+                pitch += amount;
+            },
+
+            increaseYaw: function (amount) {
+                yaw += amount;
+            },
+
+            forward: function (amount) {
+                var sp = Math.sin(pitch);
+                var cp = Math.cos(pitch);
+                var sy = Math.sin(yaw);
+                var cy = Math.cos(yaw);
+                eye[0] += amount * -cp * sy;
+                eye[1] += amount * sp;
+                eye[2] += amount * cp * cy;
+            },
+
+            left: function (amount) {
+                eye[0] += amount * Math.cos(yaw);
+                eye[2] += amount * Math.sin(yaw);
+            },
+
+            up: function (amount) {
+                eye[1] += amount;
+            }
+        };
+    }
+);
